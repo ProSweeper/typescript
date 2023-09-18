1cb4225c7175e4f3f902fe08e9c0b8918160422b
Dylan Doucet <ddoucet@local.sequiter.com>
2023-09-18 09:11:43 -0600

    start sort

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,1 +1,23 @@
 "use strict";
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    // swap left and right values if left > right
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+const array = [1, -5, 10, 4, 2];
+const sorter = new Sorter(array);
+sorter.sort();
+console.log(sorter.collection);
